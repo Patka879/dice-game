89c5a4c75a92981235f1c0238951e67996d04b34
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-07-29 15:41:43 +0200

    button function created, logic for score displaying added

--- index.js
+++ index.js
@@ -4,10 +4,33 @@ let playerOneScore = 0
 let playerTwoScore = 0
 let playerOneTurn = true
 
-const playerOneDice = document.getElementById("player1Dice").innerHTML
-const playerTwoDice = document.getElementById("player2Dice").innerHTML
+const playerOneDice = document.getElementById("player1Dice")
+const playerTwoDice = document.getElementById("player2Dice")
 const playerOneScoreboard = document.getElementById("player1Scoreboard")
 const playerTwoScoreboard = document.getElementById("player2Scoreboard")
 const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
+
+rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+
+    if (playerOneTurn) {
+        playerOneDice.textContent = randomNumber 
+        message.textContent  = "Player 2 Turn"
+        playerOneDice.classList.remove("active")
+        playerTwoDice.classList.add("active")
+        playerOneScore += randomNumber
+        playerOneScoreboard.textContent = playerOneScore
+    } else {
+        playerTwoDice.textContent = randomNumber
+        message.textContent  = "Player 1 Turn"
+        playerTwoDice.classList.remove("active")
+        playerOneDice.classList.add("active")
+        playerTwoScore += randomNumber
+        playerTwoScoreboard.textContent = playerTwoScore
+    }
+
+    playerOneTurn = !playerOneTurn 
+
+})
